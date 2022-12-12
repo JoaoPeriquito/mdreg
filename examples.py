aea7f7cb2a1bf13e420a5732797aa0ba41e9fa05
JoaoPeriquito <joao.s.periquito@gmail.com>
2022-12-12 10:00:38 +0000

    added multiprocessing deformation + model fit

--- examples.py
+++ examples.py
@@ -8,16 +8,22 @@ import pandas as pd
 
 from dbdicom.folder import Folder
 from mdreg import MDReg
-import mdreg.models as mdl
+import mdreg.models.DTI
+import mdreg.models.T2star
+import mdreg.models.DWI_monoexponential
+import mdreg.models.T1
+import mdreg.models.T2
+import mdreg.models.DCE_2CFM
+import mdreg.models.constant
+
 
 data = os.path.join(os.path.dirname(__file__), 'data')
 results = os.path.join(os.path.dirname(__file__), 'results')
 elastix_pars = os.path.join(os.path.dirname(__file__), 'elastix')
 
 # To read and write from/to other locations, set these path names
-data = 'C:\\Users\\steve\\Dropbox\\Data\\mdr_data'
-results = 'C:\\Users\\steve\\Dropbox\\Data\\mdr_results'
-
+data = 'C://Users//md1jdsp//Documents//DICOMs//test_case_iBEAt_4128009'
+results = 'C://Users//md1jdsp//Documents//DICOMs//results'
 
 
 def fit_DTI():
@@ -34,11 +40,11 @@ def fit_DTI():
     mdr = MDReg()
     mdr.set_array(np.squeeze(array[:,:,slice,:,0]))
     mdr.signal_parameters = [b_values, b_vectors, orientation]
-    mdr.signal_model = mdl.DTI
+    mdr.signal_model = mdreg.models.DTI
     mdr.pixel_spacing = header[slice,0,0].PixelSpacing
     mdr.read_elastix(os.path.join(elastix_pars, 'BSplines_DTI.txt'))
     mdr.set_elastix(MaximumNumberOfIterations = 256)
-    mdr.precision = 1
+    mdr.precision = 100
     mdr.export_path = os.path.join(results, 'DTI')
     mdr.fit()
     mdr.export()
@@ -58,10 +64,10 @@ def fit_DWI_simple():
     mdr.set_array(np.squeeze(array[:,:,slice,:,0]))
     mdr.signal_parameters = [0,10.000086, 19.99908294, 30.00085926, 50.00168544, 80.007135, 100.0008375, 199.9998135, 300.0027313, 600.0]
     mdr.pixel_spacing = header[slice,0,0].PixelSpacing
-    mdr.signal_model = mdl.DWI_simple
+    mdr.signal_model = mdreg.models.DWI_simple
     mdr.read_elastix(os.path.join(elastix_pars, 'BSplines_IVIM.txt'))
     mdr.set_elastix(MaximumNumberOfIterations = 256)
-    mdr.precision = 1
+    mdr.precision = 100
     mdr.export_path = os.path.join(results, 'DWI_simple')
     mdr.fit()
     mdr.export()
@@ -81,10 +87,10 @@ def fit_T1_simple():
     mdr.set_array(np.squeeze(array[:,:,slice,:,0]))
     mdr.signal_parameters = [hdr.InversionTime for hdr in header[slice,:,0]]
     mdr.pixel_spacing = header[slice,0,0].PixelSpacing
-    mdr.signal_model = mdl.T1_simple
+    mdr.signal_model = mdreg.models.T1
     mdr.read_elastix(os.path.join(elastix_pars, 'BSplines_T1.txt'))
     mdr.set_elastix(MaximumNumberOfIterations = 256)
-    mdr.precision = 1
+    mdr.precision = 100
     mdr.export_path = os.path.join(results, 'T1_simple')
     mdr.fit()
     mdr.export()
@@ -103,7 +109,7 @@ def fit_constant():
     mdr = MDReg()
     mdr.set_array(np.squeeze(array[:,:,slice,:,0]))
     mdr.pixel_spacing = header[slice,0,0].PixelSpacing
-    mdr.signal_model = mdl.constant
+    mdr.signal_model = mdreg.models.constant
     mdr.read_elastix(os.path.join(elastix_pars, 'BSplines_MT.txt'))
     mdr.set_elastix(MaximumNumberOfIterations = 256)
     mdr.precision = 10
@@ -126,8 +132,8 @@ def fit_T2_simple():
     mdr.set_array(np.squeeze(array[:,:,slice,:,0]))
     mdr.signal_parameters = [0,30,40,50,60,70,80,90,100,110,120]
     mdr.pixel_spacing = header[slice,0,0].PixelSpacing
-    mdr.signal_model = mdl.T2_simple
-    mdr.read_elastix(os.path.join(elastix_pars, 'BSplines_T2_simple.txt'))
+    mdr.signal_model = mdreg.models.T2
+    mdr.read_elastix(os.path.join(elastix_pars, 'BSplines_T2.txt'))
     mdr.set_elastix(MaximumNumberOfIterations = 256)
     mdr.precision = 1
     mdr.export_path = os.path.join(results, 'T2_simple')
@@ -149,7 +155,7 @@ def fit_T2star_simple():
     mdr.set_array(np.squeeze(array[:,:,slice,:,0]))
     mdr.signal_parameters = [hdr.EchoTime for hdr in header[slice,:,0]]
     mdr.pixel_spacing = header[slice,0,0].PixelSpacing
-    mdr.signal_model = mdl.T2star_simple
+    mdr.signal_model = mdreg.models.T2star
     mdr.read_elastix(os.path.join(elastix_pars, 'BSplines_T2star.txt'))
     mdr.set_elastix(MaximumNumberOfIterations = 256)
     mdr.precision = 10
@@ -175,7 +181,7 @@ def fit_DCE_2CFM_model():
     mdr = MDReg()
     mdr.set_array(np.squeeze(array[:,:,slice,:,0]))
     mdr.signal_parameters = [np.squeeze(aif[:,1]), np.squeeze(aif[:,0]), baseline, hematocrit]
-    mdr.signal_model = mdl.DCE_2CFM
+    mdr.signal_model = mdreg.models.DCE_2CFM
     mdr.fit_signal()
     mdr.export_path = os.path.join(results, 'DCEmodel')
     mdr.export_data()
@@ -191,9 +197,9 @@ if __name__ == '__main__':
 
 #    fit_DWI_simple()
 #    fit_T1_simple()
-#    fit_T2_simple()
+    fit_T2_simple()
 #    fit_T2star_simple()
 #    fit_DCE_2CFM()
 #    fit_DTI()
-    fit_constant()   
+#    fit_constant()   
     
